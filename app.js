d8b6500715605d3e2f2b3ddc3bb5e5ccfa25968f
anuj-801 <anujchaudhary88844@gmail.com>
2026-09-01 13:54:01 +0530

    implementing authentication

--- app.js
+++ app.js
@@ -8,10 +8,14 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 
 app.set("view engine", "ejs");
@@ -35,6 +39,12 @@ const sessionOptions = {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 main().
     then(() => {
         console.log("Successfull connection...");
@@ -57,8 +67,19 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.get("/demouser", async(req, res) => {
+    let fakeUser = new User({
+        email: "anuj@gmail.com",
+        username: "anuj-kumar",
+    });
+
+    let registeredUser = await User.register(fakeUser, "helloworld");
+    res.send(registeredUser);
+});
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 // app.all("*", (req, res, next) => {
 //     next(new ExpressError(404, "PAGE NOT FOUND"));
