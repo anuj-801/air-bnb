fff69011d7aea218ee03946fad60036fdc97b86b
anuj-801 <anujchaudhary88844@gmail.com>
2026-09-12 19:35:13 +0530

    Security changes

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 const sessionOptions = {
-  secret: "mysupersecretcode",
+  secret: process.env.SECERT,
   resave: false,
   saveUninitialized: true,
   cookie: {
